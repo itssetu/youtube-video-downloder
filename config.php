0360e57100eddc51043271acb471442e4a260bdd
itssetu <39484323+itssetu@users.noreply.github.com>
2018-08-04 05:58:09 -0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+$config = array();
+
+$config['site_title'] = 'youtube downloder'; // Website Title
+
+$config['site_meta'] = 'The Best Free  YouTube Video Downloading Tool'; // Site meta description
+
+$config['site_url'] = 'http://yourdomain.com'; // Site url with slash
+
+$config['enable_ads'] = false; // Enable ads? true / false
+
+$config['server_email'] = 'noreply@yourdomain.com'; // Your server email address where all emails will be sent FROM
+
+$config['contact_email'] = 'contact@gmail.com'; // Your personal email to receive all contact email
+
+$config['ads_code'] = '<div class="ads"><img src="img/ads.png" alt="ads banner"></div>' //paste your ad code into the quotemarks
+
+?>
